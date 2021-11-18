432b00d089e82c44d7e89d780377402f9a030ef7
Ozzy Simpson <ozzy@ozzysimpson.com>
2021-11-18 11:23:58 -0500

    setup

--- Sentence.java
+++ Sentence.java
@@ -1,1 +1,13 @@
+public class Sentence{
+    public static String PLANET = "Earth";
+    private static int count;
+    public String name;
 
+    private String text;
+    private String author;
+
+  public static void main(String[] args){
+
+  }
+
+}
