4118d5593291f7ed32b2f775a7ab4c3c7b18922e
Ozzy Simpson <ozzy@ozzysimpson.com>
2021-11-18 11:47:16 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,22 +1,22 @@
-//https://www.w3schools.com/java/java_files_read.asp
+// Driver code taken from https://www.w3schools.com/java/java_files_read.asp
 
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Driver{
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("testdata.csv");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("testdata.csv");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
-  }
 }
