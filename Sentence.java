851f8ec7921d3ae309536cb91481c89acd5dce75
Ozzy Simpson <ozzy@ozzysimpson.com>
2021-11-18 11:34:54 -0500

    Update Sentence.java

--- Sentence.java
+++ Sentence.java
@@ -1,13 +1,46 @@
 public class Sentence{
-    public static String PLANET = "Earth";
-    private static int count;
-    public String name;
-
     private String text;
     private String author;
+    private String timestamp;
 
-  public static void main(String[] args){
+    public Sentence(String t, String a, String ts) {    
+        text = t;
+        author = a;
+        timestamp = ts;
+    }
 
-  }
+    public String getText(){
+        return text;
+    }
+
+    public String setText(String t){
+        text = t;
+        return text;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public String setAuthor(String a){
+        author = a;
+        return author;
+    }
+
+    public String getTimestamp(){
+        return timestamp;
+    }
+
+    public String setTimestamp(String ts){
+        timestamp = ts;
+        return timestamp;
+    }
+
+    public String toString() {
+        return "{author:" + author + ", sentence:\"" + text + "\", timestamp:\"" + timestamp + "\"}";
+    }
+    public static void main(String[] args){
+
+    }
 
 }
