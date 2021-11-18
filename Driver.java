b095c0ce6a00f0da4a4a55df239e91a704fb3362
Lauren <94632367+laurenschmidt54@users.noreply.github.com>
2021-11-18 11:44:23 -0500

    Update Driver.java
    ozzy smells bad

--- Driver.java
+++ Driver.java
@@ -1,7 +1,22 @@
+//https://www.w3schools.com/java/java_files_read.asp
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class Driver{
-
-  public static void main(String[] args){
-
+  public static void main(String[] args) {
+    try {
+      File myObj = new File("testdata.csv");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
   }
-
 }
