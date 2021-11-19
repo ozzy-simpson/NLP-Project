186d44305421d462c3700ee0913b4b61d6070645
Ozzy Simpson <ozzy@ozzysimpson.com>
2021-11-18 20:06:59 -0500

    Clean up driver
    Remove old line conversion method, use convertLine

--- Driver.java
+++ Driver.java
@@ -5,57 +5,38 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
 
 public class Driver{
-    private static List<String> getRecordFromLine(String line) {
-        List<String> values = new ArrayList<String>();
-        try (Scanner rowScanner = new Scanner(line)) {
-            rowScanner.useDelimiter(",");
-            int lineNr = 1;
-            while (rowScanner.hasNext()) {
-                String value = rowScanner.next();
-                if (lineNr == 3) { // Date column
-                    value = formatDate(value);
-                }
-                if (lineNr >= 6) { // Tweet content
-                    value = removePunct(value);
-                }
-                values.add(value);
-                lineNr++;
-            }
-        }
-        return values;
+    public static Sentence convertLine(String line) {
+        String[] values = line.split("\",\"");
+        String author = values[4];
+        String timestamp = formatDate(values[2]);
+        String text = removePunct(values[5]);
+        
+        Sentence converted = new Sentence(text, author, timestamp);
+        return converted;
     }
+
     private static String formatDate(String timestamp) {
         // Example input format required: Mon May 11 03:18:03 UTC 2009
         String[] date = timestamp.split(" ");
-        return "\"" + date[1] + " " + date[2] + ", " + date[5];
+        return date[1] + " " + date[2] + ", " + date[5].replaceAll("\"","");
     }
+    
     private static String removePunct(String line) {
-        return "\"" + line.replaceAll("\\p{Punct}","") + "\"";
-    }
-
-    public static Sentence convertLine(String line) {
-
-        String[] values = line.split("\", \"");
-        String author = values[4];
-        String timestamp = values[2];
-        String text = values[5];
-        
-        Sentence converted = new Sentence(text, author, timestamp);
-
-        return converted;
+        return line.replaceAll("\\p{Punct}","");
     }
 
     public static void main(String[] args) {
         try {
-            List<List<String>> records = new ArrayList<>();
             File myObj = new File("testdata.csv");
             Scanner myReader = new Scanner(myObj);
+
+            ArrayList<Sentence> records = new ArrayList<>();
+
             while (myReader.hasNextLine()) {
-                records.add(getRecordFromLine(myReader.nextLine()));
+                records.add(convertLine(myReader.nextLine()));
             }
             System.out.println(records);
             myReader.close();
