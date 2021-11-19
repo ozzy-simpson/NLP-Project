5748e91edc5eebf86965921f15661d1f413d21fd
Ozzy Simpson <ozzy@ozzysimpson.com>
2021-11-18 21:26:01 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,6 +1,4 @@
 // Initial driver code taken from https://www.w3schools.com/java/java_files_read.asp
-// Code to convert CSV data to arraylist adapted from https://www.baeldung.com/java-csv-file-array
-// Punctuation replacement from https://www.studytonight.com/java-examples/how-to-remove-punctuation-from-string-in-java
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -39,6 +37,7 @@ public class Driver{
         }
     }
     
+    // https://www.studytonight.com/java-examples/how-to-remove-punctuation-from-string-in-java
     private static String removePunct(String line) {
         return line.replaceAll("\\p{Punct}","");
     }
