0b29426b78c47e02d0ef40c5d71c68513b7ed26a
Ozzy Simpson <ozzy@ozzysimpson.com>
2021-11-30 12:16:42 -0500

    HashMap of words

--- Driver.java
+++ Driver.java
@@ -5,25 +5,28 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.text.ParseException;
 import java.util.TimeZone;
+import java.util.Map;
+import java.util.Collections;
 
-public class Driver{
+public class Driver {
     public static Sentence convertLine(String line) {
         String[] values = line.split("\",\"");
         String author = values[4];
         String timestamp = formatDate(values[2]);
         String text = removePunct(values[5]);
-        
+
         Sentence converted = new Sentence(text, author, timestamp);
         return converted;
     }
 
     private static String formatDate(String timestamp) {
         try {
-            SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy",Locale.ENGLISH);
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
             sdf.setTimeZone(TimeZone.getTimeZone("UTC")); // https://stackoverflow.com/a/19115247
             Date date = sdf.parse(timestamp);
             SimpleDateFormat newDate = new SimpleDateFormat("MMM dd yyyy");
@@ -36,13 +39,14 @@ public class Driver{
             return timestamp;
         }
     }
-    
+
     // https://www.studytonight.com/java-examples/how-to-remove-punctuation-from-string-in-java
     private static String removePunct(String line) {
-        return line.replaceAll("\\p{Punct}","");
+        return line.replaceAll("\\p{Punct}", "");
     }
 
     public static void main(String[] args) {
+
         try {
             File myObj = new File("testdata.csv");
             Scanner myReader = new Scanner(myObj);
@@ -53,6 +57,24 @@ public class Driver{
                 records.add(convertLine(myReader.nextLine()));
             }
             myReader.close();
+
+            HashMap<String, Integer> map = new HashMap<>();
+
+            // for loop for part 3, loop through arraylist and update hashmap
+            for (int i = 0; i < records.size(); i++) {
+                Sentence tweet = records.get(i);
+                ArrayList<String> words = tweet.splitSentence(tweet.getText());
+                for (int j = 0; j < words.size(); j++) // Adapted fromhttps://stackoverflow.com/a/31289506
+                {
+                    if (map.get(words.get(j)) == null) {
+                        map.put(words.get(j), 1);
+                    } else {
+                        int newValue = Integer.valueOf(String.valueOf(map.get(words.get(j))));
+                        newValue++;
+                        map.put(words.get(j), newValue);
+                    }
+                }
+            }
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
